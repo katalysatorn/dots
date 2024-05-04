2251a0ad64227e98cdd5c25bd674cb6f5148e59c
Blair Burton <me@katalysatorn.xyz>
2024-05-04 14:53:16 +1200

    refac: merge alpha.nix and custom-hardware.nix

--- hosts/alpha/alpha.nix
+++ hosts/alpha/alpha.nix
@@ -1,10 +1,17 @@
+{ config, ... }:
+
 {
 	imports = [
-		./custom-hardware.nix
+		"${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/asus/zephyrus/ga402x/nvidia"
 		../../modules/apps/apps.nix
 		../../modules/system/de/gnome.nix
 	];
 
-	networking.hostName = "alpha";
+	config.networking.hostName = "alpha";
 
+	config.hardware.nvidia = {
+		package = config.boot.kernelPackages.nvidiaPackages.beta;
+	};
+
+	options.hardware.asus.battery.chargeUpTo = 90;
 }
