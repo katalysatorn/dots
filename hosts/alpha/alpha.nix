1f63ceba34af88e710c90b9061e4722777e46cbc
Blair Burton <me@katalysatorn.xyz>
2024-05-04 15:16:18 +1200

    wip: trying to get bluetooth working

--- hosts/alpha/alpha.nix
+++ hosts/alpha/alpha.nix
@@ -1,10 +1,14 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
 	imports = [
 		"${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/asus/zephyrus/ga402x/nvidia"
 		../../modules/apps/apps.nix
 		../../modules/system/de/gnome.nix
+	];
+
+	config.environment.systemPackages = with pkgs; [
+		bluez
 	];
 
 	config.networking.hostName = "alpha";
@@ -14,4 +18,16 @@
 	};
 
 	options.hardware.asus.battery.chargeUpTo = 90;
+
+	config.hardware.bluetooth = {
+		enable = true;
+		powerOnBoot = true;
+		settings = {
+			General = {
+				Experimental = true;
+			};
+		};
+	};
+
+	config.services.blueman.enable = true;
 }
