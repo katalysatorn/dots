d6b9b55459fbaa532e319f599d405433ef37e1a2
katalysatorn <me@katalysatorn.xyz>
2024-05-04 20:08:04 +1200

    Cleanup of nvim config - Sorry Primagen - harpoon is too confusing for me, thanks for the rest of the config though <3

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -26,7 +26,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
---  ensure_installed = { 'html', 'rust_analyzer', 'tsserver', 'eslint', 'golangci_lint_ls', 'gopls' },
+  ensure_installed = { 'html', 'rust_analyzer', 'gopls', 'markdown_oxide' },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
