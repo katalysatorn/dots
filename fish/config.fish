93967919c02adea55bb2e8215145b2b7b09e00f2
katalysatorn <me@katalysatorn.xyz>
2024-05-18 10:55:06 +1200

    Add fisher to autoinstall if not present

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
+if type -q $fisher
+	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+	fisher install jorgebucaran/fisher
+	fisher update
+end
+
 if status is-interactive
 	##
 end
@@ -5,4 +11,3 @@ end
 set fish_greeting
 
 zoxide init fish --cmd cd | source
-starship init fish | source
