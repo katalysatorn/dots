ce4576dbc06b56084035c00f34b89cff0585b0ce
katalysatorn <me@katalysatorn.xyz>
2024-05-22 13:27:07 +1200

    Remove auto install, doesnt work

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
-if type -q $fisher
-	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-	fisher install jorgebucaran/fisher
-	fisher update
-end
-
 if status is-interactive
 	##
 end
@@ -12,4 +6,3 @@ set fish_greeting
 
 zoxide init fish --cmd cd | source
 
-fish_add_path $HOME/.bun/bin
