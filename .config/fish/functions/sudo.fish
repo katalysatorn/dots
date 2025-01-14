0bb2ee52f3516a1baccf4b60f213a4106384fe2b
katalysatorn. <me@katalysatorn.xyz>
2025-01-14 19:30:48 +1300

    Fix another error with formatting

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,9 +1,9 @@
 function sudo --wraps=doas --description 'alias sudo=doas'
   doas $argv
         
-  if test -f /usr/bin/doas
-	  doas $argv
-  else
-	  /usr/bin/sudo $argv
+  if test -f /usr/bin/doas;
+	  doas $argv;
+  else;
+	  /usr/bin/sudo $argv;
   end
 end
