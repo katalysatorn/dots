b2434210eb898e98d158b79b090bdadf3a6dae99
katalysatorn <me@katalysatorn.xyz>
2025-12-02 18:10:24 +1300

    move changes

--- .config/hypr/scripts/nvidia.sh
+++ .config/hypr/scripts/nvidia.sh
@@ -1,10 +1,10 @@
-echo > /tmp/hyprland-supergfxctl-script-user
+echo > $HOME/.cache/hyprland-supergfxctl-script-user
 
 if [ "$(supergfxctl -g)" = "Hybrid" ]; then
-	echo "# Running in hybrid" >> /tmp/hyprland-supergfxctl-script-user
-	echo "env = AQ_DRM_DEVICES, /dev/dri/card1" >> /tmp/hyprland-supergfxctl-script-user
+	echo "# Running in hybrid" >> $HOME/.cache/hyprland-supergfxctl-script-user
+	echo "env = AQ_DRM_DEVICES, /dev/dri/card1" >> $HOME/.cache/hyprland-supergfxctl-script-user
 elif [ "$(supergfxctl -g)" = "AsusMuxDgpu" ]; then
-	echo "# Running in mux" >> /tmp/hyprland-supergfxctl-script-user
-	echo "env = LIBVA_DRIVER_NAME, nvidia" >> /tmp/hyprland-supergfxctl-script-user
-	echo "env = __GLX_VENDOR_LIBRARY_NAME, nvidia" >> /tmp/hyprland-supergfxctl-script-user
+	echo "# Running in mux" >> $HOME/.cache/hyprland-supergfxctl-script-user
+	echo "env = LIBVA_DRIVER_NAME, nvidia" >> $HOME/.cache/hyprland-supergfxctl-script-user
+	echo "env = __GLX_VENDOR_LIBRARY_NAME, nvidia" >> $HOME/.cache/hyprland-supergfxctl-script-user
 fi
